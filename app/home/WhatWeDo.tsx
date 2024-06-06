47e840a8678a8b19cfbadfc4ef0bdd14def092f7
Muhammad Ehsan <muhammadehsanhassan781@gmail.com>
2024-06-06 11:51:43 +0500

    Add changes in home responsive

--- app/home/WhatWeDo.tsx
+++ app/home/WhatWeDo.tsx
@@ -5,12 +5,12 @@ export default function WhatWeDo() {
         <div className="text-center font-poppins">
           <div className="flex items-center gap-2 md:gap-8 justify-center uppercase font-bold text-lg md:text-2xl lg:text-3xl xl:text-5xl leading-[20px] md:leading-[50px] lg:leading-[69px] tracking-[1.5%]">
             {/* <div className="h-[7px] w-[100px] md:w-[160px] rounded-lg bg-gradient-to-r  via-customTeal to-customBlue1"></div> */}
-            <img src="/hori-line.svg" alt="" />
+            <img src="/hori-line.svg" className='w-[90px] h-[9px] md:w-auto md:h-auto' alt="" />
             <div>
               <span className="text-customBlue1">What</span>{" "}
               <span className="text-customDarkGray">we do?</span>
             </div>
-            <img src="/hori-line2.svg" alt="" />
+            <img src="/hori-line2.svg" className='w-[90px] h-[9px] md:w-auto md:h-auto' alt="" />
             {/* <div className="h-[7px] w-[100px] md:w-[160px] rounded-lg bg-gradient-to-r from-customBlue1 via-customTeal"></div> */}
           </div>
           <div className="mb-4 md:mb-[25px] text-[18px] md:text-[20px] lg:text-[22px] font-light leading-[32.78px] tracking-[2%]">
