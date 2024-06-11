a101df4e63e25987782acde792d9a34d8f4ec555
Muhammad Ehsan <muhammadehsanhassan781@gmail.com>
2024-06-11 19:04:05 +0500

    Add changes in Our portfolio

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from './Hero'
 import OurTeam from './OurTeam'
-import OurTeam2 from './OurTeam2'
+import OurTeam2 from './OurTeam-second-template'
 export default function Team() {
   return (
     <>
