fbfff96bf83452472d31686b428d52bc5d693935
Muhammad Ehsan <muhammadehsanhassan781@gmail.com>
2024-06-24 15:52:31 +0500

    Add animations in hero sections of services, portfolio, team page

--- app/services/Hero.tsx
+++ app/services/Hero.tsx
@@ -11,15 +11,15 @@ export default function Hero() {
       <div className="flex flex-col md:flex-row justify-center">
         <div className="pl-4 lg:pl-8 xl:pl-8 2xl:pl-20 flex justify-start items-start md:items-center 2xl:items-start md:basis-[45%] lg:basis-1/2">
           <div className="py-8 md:py-12 2xl:pt-24 flex flex-col basis-[90%] md:basis-[86%] md:pl-4 2xl:basis-[72%]">
-            <div className="uppercase font-poppins font-bold text-2xl md:text-xl lg:text-3xl xl:text-4xl 2xl:text-[50px] leading-[35px] md:leading-[30px] lg:leading-[40px] lg:mb-2 xl:leading-[45px] 2xl:leading-[69px] tracking-[1.5%]">
+            <div className="fly-in-left uppercase font-poppins font-bold text-2xl md:text-xl lg:text-3xl xl:text-4xl 2xl:text-[50px] leading-[35px] md:leading-[30px] lg:leading-[40px] lg:mb-2 xl:leading-[45px] 2xl:leading-[58px] 3xl:leading-[69px] tracking-[1.5%]">
               <span className="text-[#ffffff]">Our Diverse Range of IT</span>{" "}
               <span className="text-[#00FFFF]">Services</span>
             </div>
-            <p className="mb-4 md:mb-2 lg:mb-4 2xl:mb-[25px] font-poppins font-light text-[16px] md:text-[13.3px] lg:text-[16px] xl:text-[18px] 2xl:text-[22px] leading-[24px] md:leading-[24px] xl:leading-[32.78px] tracking-[2%]">
+            <p className="fly-in-1-5s mb-4 md:mb-2 lg:mb-4 2xl:mb-[25px] font-poppins font-light text-[16px] md:text-[13.3px] lg:text-[16px] xl:text-[18px] 2xl:text-[22px] leading-[24px] md:leading-[24px] xl:leading-[32.78px] tracking-[2%]">
               Elevate your digital presence with our diverse IT services, from
               web development to innovative software solutions.
             </p>
-            <div className="xl:mb-8">
+            <div className="fly-in-2s xl:mb-8">
               
               <Link href="/hireus" className={` ${pathname === "/hireus"}`}>
                 <Button customClass="text-white font-medium text-[21.72px] leading-[32.58px] py-2 px-4 bg-gradient-to-t from-customBlue2 to-customBlue3 h-[55.26px] rounded-[8.45px] hover:transition-transform hover:duration-300 hover:ease-in-out hover:shadow-[0_0_15px_#005BA1]">
--- app/team/Hero.tsx
+++ app/team/Hero.tsx
@@ -10,17 +10,17 @@ export default function Hero() {
       <div className="flex flex-col md:flex-row justify-center ">
         <div className="flex justify-start pl-4 md:pl-8 lg:pl-12 xl:pl-[3rem] 2xl:pl-[6rem] items-start md:items-center 2xl:items-start md:basis-[45%] lg:basis-1/2">
           <div className=" md:pl-0 md:py-12 2xl:pt-44 flex flex-col basis-[90%] md:basis-[86%] 2xl:basis-[80%]">
-            <div className="uppercase font-poppins font-bold text-2xl md:text-xl lg:text-3xl xl:text-4xl 2xl:text-[50px] leading-[35px] md:leading-[30px] lg:leading-[40px] lg:mb-2 xl:leading-[45px] 2xl:leading-[69px] tracking-[1.5%]">
+            <div className="fly-in-left uppercase font-poppins font-bold text-2xl md:text-xl lg:text-3xl xl:text-4xl 2xl:text-[50px] leading-[35px] md:leading-[30px] lg:leading-[40px] lg:mb-2 xl:leading-[45px] 2xl:leading-[69px] tracking-[1.5%]">
               <span className="text-[#ffffff]">
                 Meet Our Dynamic <span className="text-[#00FFFF]">Team</span> of
                 Tech <span className="text-[#00FFFF]">Innovators</span>
               </span>
             </div>
-            <p className="mb-4 md:mb-2 lg:mb-4 2xl:mb-[25px] font-poppins font-light text-[16px] md:text-[13.3px] lg:text-[16px] xl:text-[18px] 2xl:text-[22px] leading-[24px] md:leading-[24px] xl:leading-[32.78px] tracking-[2%]">
+            <p className="fly-in-1-5s mb-4 md:mb-2 lg:mb-4 2xl:mb-[25px] font-poppins font-light text-[16px] md:text-[13.3px] lg:text-[16px] xl:text-[18px] 2xl:text-[22px] leading-[24px] md:leading-[24px] xl:leading-[32.78px] tracking-[2%]">
               Discover the Brilliance of Our Tech Team, Crafting Innovative
               Solutions for Your Success.
             </p>
-            <div className="xl:mb-8">
+            <div className="fly-in-2s xl:mb-8">
               
               <Link href="/hireus" className={` ${pathname === "/hireus"}`}>
                 <Button customClass="text-white font-medium text-[21.72px] leading-[32.58px] py-2 px-4 bg-gradient-to-t from-customBlue2 to-customBlue3 h-[55.26px] rounded-[8.45px] hover:transform hover:transition-transform hover:duration-300 hover:ease-in-out hover:shadow-[0_0_15px_#005BA1]">
