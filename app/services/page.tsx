83d00958598e50092aa42067742f991475b8dca5
Muhammad Ehsan <muhammadehsanhassan781@gmail.com>
2024-06-27 18:28:36 +0500

    revert code in services page.tsx and ourservices.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,23 +1,13 @@
-"use client"
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router';
+import React from 'react'
 import Hero from './Hero'
 import OurServices from './OurServices'
+
 export default function Services() {
-  const router = useRouter();
-  const { tab } = router.query;
-  const selectedTab = typeof tab === 'string' ? tab : null;
-
-  useEffect(() => {
-    if (selectedTab) {
-      document.getElementById(selectedTab)?.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [selectedTab]);
-
   return (
-    <>
-      <Hero />
-      <OurServices selectedTab={selectedTab} />
-    </>
-  );
+       <>
+     
+      <Hero/>
+      <OurServices/>
+    </>
+  )
 }
