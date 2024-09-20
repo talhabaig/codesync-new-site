3136b5f87b87cdb99509f9d6ede2571d3b8ccd5a
Muhammad Ehsan <muhammadehsanhassan781@gmail.com>
2024-09-20 18:46:59 +0500

    Add changes in env

--- app/components/blogEditor.tsx
+++ app/components/blogEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic'; 
 import 'react-quill/dist/quill.snow.css';
 // import useCustomClipboard from './customclipboard';
 
