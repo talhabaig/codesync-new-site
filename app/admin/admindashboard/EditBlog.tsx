0ec053d8f42ae1e01cec789a1293da142ba20b2a
Muhammad Ehsan <muhammadehsanhassan781@gmail.com>
2024-09-23 11:25:00 +0500

    Add new env variables on vercal

--- app/admin/admindashboard/EditBlog.tsx
+++ app/admin/admindashboard/EditBlog.tsx
@@ -1,4 +1,4 @@
-
+"use client";
 import React, { useState, useEffect } from 'react';
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { storage } from '@/app/firebase/config';
